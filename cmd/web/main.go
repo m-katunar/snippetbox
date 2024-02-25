b0549992228b71fe50fb98ec0eb1e6779e91bf67
Mirko Katunar <mirko.katunar@gmail.com>
2024-02-25 10:50:53 +0100

    Setting up MySQL

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,28 +18,46 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
+	dsn := flag.String("dsn", "web:pass@(localhost:3306)/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	filesServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", filesServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	logger.Info("starting server", slog.String("addr", ":4000"))
 
-	err := http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
