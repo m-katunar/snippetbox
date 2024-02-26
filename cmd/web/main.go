a7eea819a593501eccd98d54ff85ee5fd5a51c30
Mirko Katunar <mirko.katunar@gmail.com>
2024-02-26 22:40:53 +0100

    Designing a database model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,12 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/m-katunar/snippetbox/internal/models"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -33,7 +35,8 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", slog.String("addr", ":4000"))
