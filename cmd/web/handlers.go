572b1b5c8a5e8fbdebbb4d7ba58e8eadbd488e58
Mirko Katunar <mirko.katunar@gmail.com>
2024-04-06 09:49:57 +0200

    User authorization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -227,14 +227,23 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Add the ID of the current user to the session, so that they are now
-	// 'logged in'.
 	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
-	// Redirect the user to the create snippet page.
+
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
