07076ecb9c36c5b430419c6baed67aacb2b0b12c
Mirko Katunar <mirko.katunar@gmail.com>
2024-04-07 16:46:47 +0200

    csrf protection

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -51,6 +53,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
