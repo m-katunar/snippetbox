6ceba45bde82f084c3c63567669a19f5b8a34f1b
Mirko Katunar <mirko.katunar@gmail.com>
2024-04-20 11:47:08 +0200

    File embedding

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"github.com/m-katunar/snippetbox/internal/models"
+	"github.com/m-katunar/snippetbox/ui"
 )
 
 type templateData struct {
@@ -27,35 +29,32 @@ var functions = template.FuncMap{
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-	// Initialize a new map to act as the cache.
+
 	cache := map[string]*template.Template{}
 	// Use the filepath.Glob() function to get a slice of all filepaths that
 	// match the pattern "./ui/html/pages/*.tmpl". This will essentially gives
 	// us a slice of all the filepaths for our application 'page' templates
 	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl.html")
 	if err != nil {
 		return nil, err
 	}
-
 	for _, page := range pages {
 		name := filepath.Base(page)
+		// Create a slice containing the filepath patterns for the templates we
+		// want to parse.
+		patterns := []string{
+			"html/base.tmpl.html",
+			"html/partials/*.tmpl.html",
+			page,
+		}
+		// Use ParseFS() instead of ParseFiles() to parse the template files
+		// from the ui.Files embedded filesystem.
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
+		if err != nil {
+			return nil, err
+		}
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
-		if err != nil {
-			return nil, err
-		}
-		// Call ParseGlob() *on this template set* to add any partials.
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
-		if err != nil {
-			return nil, err
-		}
-		// Call ParseFiles() *on this template set* to add the
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
-		// Add the template set to the map as normal...
 		cache[name] = ts
 	}
 	// Return the map.
